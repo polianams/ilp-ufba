587c7e119f5524a437cc0e01a9c1b92c8ba35769
polianams <pmrtins@gmail.com>
2023-08-05 01:00:49 -0300

    adc mais exercícios

---
+++ 25-grenais/grenais.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    
+  int i, g, vi, vg, e, j, n;
+
+  n=0;
+  vi=0;
+  vg=0;
+  e=0;
+  j=0;
+
+  while (n<2)
+  {
+    cin >> i >> g;
+
+    if(i>g)
+    {
+      vi=vi+1;
+    }
+    else if(i<g)
+    {
+      vg=vg+1;
+    }
+    else
+    {
+      e=e+1;
+    }
+    
+    j=j+1;
+
+    cout << "Novo grenal (1-sim 2-nao)" << endl;
+    cin >> n;
+  }
+
+  cout << j << " grenais" << endl;
+  cout << "Inter:" << vi << endl;
+  cout << "Gremio:" << vg << endl;
+  cout << "Empates:" << e << endl;
+
+  if(vi>vg)
+  {
+  cout << "Inter venceu mais" << endl;
+  }
+
+  else if(vi<vg)
+  {
+  cout << "Gremio venceu mais" << endl;
+  }
+
+  else
+  {
+  cout << "Nao houve vencedor" << endl;
+  }
+
+  return 0;
+}
